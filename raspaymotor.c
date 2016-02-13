5a5c1fc257a94e2c81506d413b6413361028b637
EsseiK <esseikuroda@gmail.com>
2016-02-13 22:40:51 +0900

    3rd commit

--- raspaymotor.c
+++ raspaymotor.c
@@ -1,98 +1,28 @@
 /*モータ制御*/
 
-#include <wiringPi.h>
-#include <stdio.h>
+//#include <wiringPi.h>
 
-/*.h予定*/
+/*
+#include "stdafx.h"
+#ifndef dprintf(s,...)
+ #define dprintf(s,...) printf("[%s] " s, __func__,__VA_ARGS__)
+#else
+#endif
+*/
+#include <stdio.h>
+#include "raspi.h"
+
 /*右モータPIN*/
-#define RIGHTMOTORADVANCEPIN        //前進
-#define RIGHTMOTORREVERSEPIN        //後進
+#define RIGHTMOTORADVANCEPIN  11      //前進 /*値は適当*/
+#define RIGHTMOTORREVERSEPIN  21      //後進 /*値は適当*/
 
 /*左モータPIN*/
-#define LEFTMOTORADVANCEPIN        //前進
-#define LEFTMOTORREVERSEPIN        //後進
+#define LEFTMOTORADVANCEPIN  10      //前進 /*値は適当*/
+#define LEFTMOTORREVERSEPIN  20      //後進 /*値は適当*/
 
-/*コマンドチェック用*/
-#define COMMANDCHECK_MIN 2
-#define COMMANDCHECK_MAX 8
-#define ROTATIONBORDER 5
 
-/*回転方向*/
-#define ADVANCE 1
-#define REVERSE 2
 
-/*初期化用*/
-#define CHARDATA0 0
-#define INTDATA0 0
-#define SHORT0 0
 
-/*出力PWM*/
-#define PWMOUT0 0
-#define PWMOUT30 300
-#define PWMOUT60 600
-#define PWMOUT90 900
-
-/*出力レベル*/
-typedef enum
-{
-    OUTLEVEL0 = 0,
-    OUTLEVEL1,
-    OUTLEVEL2,
-    OUTLEVEL3,
-}
-
-/*エラーの種類*/
-typedef enum
-{
-    NONEERROR = 0,
-    COMMANDERROR,
-    PREPAREERROR,
-    COMMANDTRANSERROR,
-    PWMOUTCALCERROR,
-    PWMOUTERROR,
-    MAXERROR
-}
-
-/*モータID*/
-typedef enum 
-{
-    MOTOR_LEFT = 0,
-    MOTOR_RIGHT,
-    MOTOR_MAX
-}
-
-/*モータ状態情報の構造体*/
-typedef struct motor_state_info 
-{
-    unsigned short id;					//tt
-    unsigned short level;
-    unsigned short rotation_direction;
-    unsigned short pwm_out_value;
-    unsigned short pwm_advance_pin;		//tt
-	unsigned short pwm_reverse_pin;		//tt
-}motor_state_info;
-
-/*コマンド情報の構造体*/
-typedef struct command_info
-{
-    char large_type;
-    char small_type;
-    char spare3;
-    char left_command;
-    char right_command;
-    char OP3;
-    char OP4;
-    char spare4;
-    char error_code1;
-    char error_code2;
-}command_info;
-/*.hここまで*/
-
-int Motor_main(command_info* command);
-int Motor_CommandCheck(command_info* command);
-int Motor_Prepare(command_info command, motor_state_info motor_state , char motor_kind)
-int Motor_CommandTrans(char command_level,motor_state_info* motor_state)
-int Motor_OutCalc(motor_state_info* motor_state)
 
 /*モータ情報の初期化は全体のメイン処理の中で*/
 /*左モータ情報の初期化*/
@@ -101,6 +31,8 @@ int Motor_Init_Left(motor_state_info* motor_state_left )
 	motor_state_left -> id = MOTOR_LEFT;
 	motor_state_left -> pwm_advance_pin = LEFTMOTORADVANCEPIN;
 	motor_state_left -> pwm_reverse_pin = LEFTMOTORREVERSEPIN;
+	
+	return 0;
 }
 
 /*右モータ情報の初期化*/
@@ -109,49 +41,22 @@ int Motor_Init_Right(motor_state_info* motor_state_right)
 	motor_state_right -> id = MOTOR_RIGHT;
 	motor_state_right -> pwm_advance_pin = RIGHTMOTORADVANCEPIN;
 	motor_state_right -> pwm_reverse_pin = RIGHTMOTORREVERSEPIN;
+
+	return 0;
 }
 /*ここまで*/
 
 
-/*モータ制御のメイン処理を実施する。*/
-int Motor_main(command_info* command)
-{
-    int error_check;
-    int motor_index;
-    motor_state_info motor_state_left;
-    motor_state_info motor_state_right;
-
-    motor_state_left -> id = MOTOR_LEFT;
-    motor_state_right -> id = MOTOR_RIGHT;
-    error_check = NONEERROR;
-    
-	Motor_Init_Left(MOTOR_LEFT);
-	Motor_Init_Right(motor_state_right);
-	
-    Motor_CommandCheck(command);
-    
-    if (error_check = NONEERROR)
-    {
-        Motor_Prepare(command,motor_state_left);
-    }
-
-    if (error_check = NONEERROR)
-    {
-        Motor_Prepare(command,motor_state_right);
-    }
-    
-    return 0;
-    
-}
 
 /*左のコマンド値、右のコマンド値が2～8の値で収まっていることをチェックする。*/
 /*不備がある場合はエラー値を返す、ない場合は0を返す。*/
-int Motor_CommandCheck(command)
+int Motor_CommandCheck(command_info* command)
 {
     int error_check;
     error_check = NONEERROR;
     
-    if((command->left_command < COMMANDCHECK_MIN)||(COMMANDCHECK_MAX < command->left_command))
+    if((command->left_command < COMMANDCHECK_MIN)
+     ||(COMMANDCHECK_MAX < command->left_command))
     {
         error_check = COMMANDERROR;
     }
@@ -165,42 +70,7 @@ int Motor_CommandCheck(command)
     
 }
 
-/*モータ出力前の標準となる(コマンド処理&出力値計算の実施)*/
-/*左右のモータそれぞれで処理を行う。*/
-int Motor_Prepare(command_info command, motor_state_info motor_state)
-{
 
-    char level;
-    int errorcheck;
-
-    error_check = NONEERROR;
-    level = CHARDATA0;
-    
-    switch (motor_state -> id)
-    {
-        case MOTOR_LEFT:
-            level = command -> left_command;
-            break;
-        case MOTOR_RIGHT:
-            level = command -> right_command;
-            break;
-    default:
-            error_check = PREPAREERROR;
-            break;
-    }
-
-    if(errorcheck = NONEERROR)
-    {
-        errorcheck = Motor_CommandTrans(level,motor_state);
-        if(errorcheck = NONEERROR)
-        {
-            errorcheck = Motor_OutCalc(motor_state);
-        }
-    }
-    
-    return errorcheck;
-    
-}
 
 /*変換対象のコマンド値をもとにモータ状態レベルと向きを算出し、状態を保持する。*/
 int Motor_CommandTrans(char command_level,motor_state_info* motor_state)
@@ -251,14 +121,52 @@ int Motor_OutCalc(motor_state_info* motor_state)
             error_check = PWMOUTERROR;
             break;
     }
-    else
+    
+//    {
+//        error_check = PWMOUTCALCERROR;
+//    }
+    
+    return error_check;
+    
+}
+
+/*モータ出力前の標準となる(コマンド処理&出力値計算の実施)*/
+/*左右のモータそれぞれで処理を行う。*/
+int Motor_Prepare(command_info *command, motor_state_info *motor_state)
+{
+
+    char level;
+    int error_check;
+
+    error_check = NONEERROR;
+    level = CHARDATA0;
+    
+    switch (motor_state -> id)
     {
-        error_check = PWMOUTCALCERROR;
+        case MOTOR_LEFT:
+            level = command -> left_command;
+            break;
+        case MOTOR_RIGHT:
+            level = command -> right_command;
+            break;
+    default:
+            error_check = PREPAREERROR;
+            break;
+    }
+
+    if(error_check == NONEERROR)
+    {
+        error_check = Motor_CommandTrans(level,motor_state);
+        if(error_check == NONEERROR)
+        {
+            error_check = Motor_OutCalc(motor_state);
+        }
     }
     
     return error_check;
     
 }
+
 
 /**/
 int Motor_Output(motor_state_info* motor_state_left,motor_state_info* motor_state_right)
@@ -268,19 +176,19 @@ int Motor_Output(motor_state_info* motor_state_left,motor_state_info* motor_stat
 	{
 		if ((motor_state_left -> rotation_direction) == ADVANCE)
 		{
-    		pwmWrite( motor_state_left -> pwm_out_value, motor_state_left -> pwm_advance_pin);
-    		pwmWrite( PWMOUT0, motor_state_left -> pwm_reverse_pin);
+    		//pwmWrite( motor_state_left -> pwm_out_value, motor_state_left -> pwm_advance_pin);
+    		//pwmWrite( PWMOUT0, motor_state_left -> pwm_reverse_pin);
 		}
 		else if ((motor_state_left -> rotation_direction) == REVERSE)
 		{
-    		pwmWrite( PWMOUT0, motor_state_left -> pwm_advance_pin);
-    		pwmWrite( motor_state_left -> pwm_out_value, motor_state_left -> pwm_reverse_pin);
+    	//	pwmWrite( PWMOUT0, motor_state_left -> pwm_advance_pin);
+    	//	pwmWrite( motor_state_left -> pwm_out_value, motor_state_left -> pwm_reverse_pin);
 		}
 	}
 	else
 	{
-    	pwmWrite( PWMOUT60, motor_state_left -> pwm_advance_pin);
-    	pwmWrite( PWMOUT60, motor_state_left -> pwm_reverse_pin);
+//    	pwmWrite( PWMOUT60, motor_state_left -> pwm_advance_pin);
+  //  	pwmWrite( PWMOUT60, motor_state_left -> pwm_reverse_pin);
 	}
 	
 	
@@ -288,18 +196,122 @@ int Motor_Output(motor_state_info* motor_state_left,motor_state_info* motor_stat
 	{
 		if ((motor_state_right -> rotation_direction) == ADVANCE)
 		{
-    		pwmWrite( motor_state_right -> pwm_out_value, motor_state_right -> pwm_advance_pin);
-    		pwmWrite( PWMOUT0, motor_state_right -> pwm_reverse_pin);
+    //		pwmWrite( motor_state_right -> pwm_out_value, motor_state_right -> pwm_advance_pin);
+    //		pwmWrite( PWMOUT0, motor_state_right -> pwm_reverse_pin);
 		}
 		else if ((motor_state_right -> rotation_direction) == REVERSE)
 		{
-    		pwmWrite( PWMOUT0, motor_state_right -> pwm_advance_pin);
-    		pwmWrite( motor_state_right -> pwm_out_value, motor_state_right -> pwm_reverse_pin);
+    //		pwmWrite( PWMOUT0, motor_state_right -> pwm_advance_pin);
+    //		pwmWrite( motor_state_right -> pwm_out_value, motor_state_right -> pwm_reverse_pin);
 		}
 	}
 	else
 	{
-    	pwmWrite( PWMOUT60, motor_state_right -> pwm_advance_pin);
-    	pwmWrite( PWMOUT60, motor_state_right -> pwm_reverse_pin);
+    //	pwmWrite( PWMOUT60, motor_state_right -> pwm_advance_pin);
+    //	pwmWrite( PWMOUT60, motor_state_right -> pwm_reverse_pin);
 	}
+	
+	return 0;
+}
+
+
+/*モータ制御のメイン処理を実施する。*/
+int Motor_main(command_info* command)
+{
+    int error_check;
+    int motor_index;
+    motor_state_info motor_state_left;
+    motor_state_info motor_state_right;
+	
+    motor_state_left.id = MOTOR_LEFT;
+    motor_state_right.id = MOTOR_RIGHT;
+    error_check = NONEERROR;
+    
+	// left motor inti
+	error_check = Motor_Init_Left(&motor_state_right);
+	
+	if(error_check<0){
+		return -1;
+	}else{
+			printf("Motor_Init_Left do\r\n");	
+	}
+	
+	error_check = NONEERROR;
+
+	// rught motor inti
+	error_check = Motor_Init_Right(&motor_state_right);
+	if(error_check<0){
+		return -2;
+	}else{
+			printf("Motor_Init_Right do\r\n");	
+	}
+	error_check =    Motor_CommandCheck(command);
+    
+	
+	if (error_check != NONEERROR)
+    {
+    	return -3;
+    	
+    }else{
+    error_check = Motor_Prepare(command,&motor_state_left);
+
+    }
+	printf("Motor_Prepare left do\r\n");	
+
+    if (error_check != NONEERROR)
+    {
+    	return -4;
+
+    }else{
+    	error_check = Motor_Prepare(command,&motor_state_right);
+
+    }
+	printf("Motor_Prepare right do\r\n");	
+	
+	
+	//motor control do
+
+	error_check = Motor_Output(&motor_state_left, &motor_state_right);
+    
+	if(	error_check <0){
+		printf("Motor_Output ret = %d\r\n",error_check);
+	}else{
+		printf("Motor_Output do\r\n");	
+	}
+    return 0;
+    
+}
+void main(){
+	
+	int ret = 0;
+	command_info info;
+	
+	char tmp[11]="M001234000\0";
+	
+	int i=0;
+    info.large_type	=	tmp[i];
+	i++;
+    info.small_type	=	tmp[i];
+	i++;
+    info.spare3	=	tmp[i];
+	i++;
+    info.left_command	=	tmp[i];
+	i++;
+    info.right_command	=	tmp[i];
+	i++;
+    info.OP3	=	tmp[i];
+	i++;
+    info.OP4	=	tmp[i];
+	i++;
+    info.spare4	=	tmp[i];
+	i++;
+    info.error_code1	=	tmp[i];
+	i++;
+    info.error_code2	=	tmp[i];
+	
+	printf("%c\r\n",info.large_type);	
+	printf("%c\r\n",info.spare4);	
+	ret = Motor_main(&info);
+	
+	printf("Motor_main do ret =%d\r\n",ret);
 }
---
+++ raspi.h
@@ -0,0 +1,76 @@
+/*.h予定*/
+
+/*コマンドチェック用*/
+#define COMMANDCHECK_MIN 2
+#define COMMANDCHECK_MAX 8
+#define ROTATIONBORDER 5
+
+/*回転方向*/
+#define ADVANCE 1
+#define REVERSE 2
+
+/*初期化用*/
+#define CHARDATA0 0
+#define INTDATA0 0
+#define SHORT0 0
+
+/*出力PWM*/
+#define PWMOUT0 0
+#define PWMOUT30 300
+#define PWMOUT60 600
+#define PWMOUT90 900
+
+/*出力レベル*/
+typedef enum
+{
+    OUTLEVEL0 = 0,
+    OUTLEVEL1,
+    OUTLEVEL2,
+    OUTLEVEL3,
+}LEVEL;
+
+/*エラーの種類*/
+typedef enum
+{
+    NONEERROR = 0,
+    COMMANDERROR,
+    PREPAREERROR,
+    COMMANDTRANSERROR,
+    PWMOUTCALCERROR,
+    PWMOUTERROR,
+    MAXERROR
+}ERR;
+
+/*モータID*/
+typedef enum 
+{
+    MOTOR_LEFT = 0,
+    MOTOR_RIGHT,
+    MOTOR_MAX
+}MID;
+
+/*モータ状態情報の構造体*/
+typedef struct
+{
+    unsigned short id;					//tt
+    unsigned short level;
+    unsigned short rotation_direction;
+    unsigned short pwm_out_value;
+    unsigned short pwm_advance_pin;		//tt
+	unsigned short pwm_reverse_pin;		//tt
+}motor_state_info;
+
+/*コマンド情報の構造体*/
+typedef struct{
+    char large_type;
+    char small_type;
+    char spare3;
+    char left_command;
+    char right_command;
+    char OP3;
+    char OP4;
+    char spare4;
+    char error_code1;
+    char error_code2;
+}command_info;
+/*.hここまで*/
